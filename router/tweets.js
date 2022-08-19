904598cb3985a5a3713064a2f870a904b24416db
임빛나 <devbit4@gmail.com>
2022-08-20 06:13:12 +0900

    rest api 구현

--- router/tweets.js
+++ router/tweets.js
@@ -41,9 +41,45 @@ router.get('/:id', (req, res, next) => {
   }
 });
 
-// router.get('/', (req, res, next) => {});
+//POST /tweets
+
+router.post('/', (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    name,
+    username,
+    createdAt: new Date(),
+  };
+
+  tweets = [tweet, ...tweets];
+
+  res.status(201).json(tweet);
+});
 
 //PUT /tweets/id
 
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+
+  const tweet = tweets.find((tweet) => tweet.id === id);
+
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id${id} not found` });
+  }
+});
+
 //DELETE /tweets/id
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
+
 export default router;
